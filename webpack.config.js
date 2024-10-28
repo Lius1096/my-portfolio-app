d3d62b60cb0e3f370055c02e625b95b19a664482
Julius <julius.djossou@outlook.fr>
2024-10-29 00:32:12 +0100

    configuration de webpack et babel puis ajout du fichier html

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',  // Le fichier d'entrée de votre application
+    output: {
+        filename: 'bundle.js',  // Nom du fichier de sortie
+        path: path.resolve(__dirname, 'dist')  // Dossier de sortie
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Fichiers JavaScript
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    require('tailwindcss'),
+                                    require('autoprefixer')
+                                ]
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // Spécifiez le chemin vers votre fichier HTML
+            filename: 'index.html' // Nom du fichier généré dans le dossier de sortie
+        })
+    ],
+    mode: 'development'  // Mode (peut aussi être 'production' pour un build optimisé)
+};
