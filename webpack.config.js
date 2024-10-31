e662189db523e4ca8035ef9ef64907dad5e550d7
Julius <julius.djossou@outlook.fr>
2024-11-01 00:20:19 +0100

    Prepare for Heroku deployment

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,41 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',  // Le fichier d'entrée de votre application
-    output: {
-        filename: 'bundle.js',  // Nom du fichier de sortie
-        path: path.resolve(__dirname, 'dist')  // Dossier de sortie
+  entry: './src/index.js', // Chemin d'entrée de votre application
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Dossier de sortie
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'), // Répertoire des fichiers statiques
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/, // Fichiers JavaScript
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    require('tailwindcss'),
-                                    require('autoprefixer')
-                                ]
-                            }
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html', // Spécifiez le chemin vers votre fichier HTML
-            filename: 'index.html' // Nom du fichier généré dans le dossier de sortie
-        })
+    port: 3000,
+    open: true,
+    hot: true,
+    setupMiddlewares: (middlewares, devServer) => {
+      if (!devServer) {
+        throw new Error('Webpack Dev Server is not defined');
+      }
+      // Vous pouvez ajouter ici des middlewares supplémentaires si nécessaire
+      return middlewares;
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader', // Pour utiliser Babel avec Webpack
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'], // Chargement des fichiers CSS avec PostCSS
+      },
     ],
-    mode: 'production'  // Mode (peut aussi être 'production' pour un build optimisé)
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Permet de résoudre les extensions
+  },
 };
